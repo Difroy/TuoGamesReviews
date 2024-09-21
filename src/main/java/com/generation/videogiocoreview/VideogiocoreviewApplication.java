c4e7ea44258856fccd652c0352c601c712199601
Difroy <unifroy@gmail.com>
2024-09-21 16:11:43 +0200

    Init

---
+++ src/main/java/com/generation/videogiocoreview/VideogiocoreviewApplication.java
@@ -0,0 +1,13 @@
+package com.generation.videogiocoreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogiocoreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogiocoreviewApplication.class, args);
+	}
+
+}
