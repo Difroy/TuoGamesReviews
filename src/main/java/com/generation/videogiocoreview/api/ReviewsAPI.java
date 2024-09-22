4b680a070db30a3d43c0429860a57f73df7be87e
Difroy <unifroy@gmail.com>
2024-09-22 18:06:07 +0200

    Create ReviewsAPI.java

---
+++ src/main/java/com/generation/videogiocoreview/api/ReviewsAPI.java
@@ -0,0 +1,15 @@
+package com.generation.videogiocoreview.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("gamesreview/api/review")
+public class ReviewsAPI {
+
+	
+	
+	
+	
+	
+}
