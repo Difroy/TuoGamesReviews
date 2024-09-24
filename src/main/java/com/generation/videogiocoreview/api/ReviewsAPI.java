9c37d70e145bd81b9fded0cac62e8b4a12bf7921
Difroy <unifroy@gmail.com>
2024-09-24 10:45:01 +0200

    Update ReviewsAPI.java

--- src/main/java/com/generation/videogiocoreview/api/ReviewsAPI.java
+++ src/main/java/com/generation/videogiocoreview/api/ReviewsAPI.java
@@ -2,11 +2,8 @@ package com.generation.videogiocoreview.api;
 
 import java.util.List;
 import java.util.Optional;
-
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +17,6 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.videogiocoreview.model.dto.ReviewsDTO;
 import com.generation.videogiocoreview.model.dto.ReviewsMapper;
-import com.generation.videogiocoreview.model.entities.Games;
 import com.generation.videogiocoreview.model.entities.Reviews;
 import com.generation.videogiocoreview.model.repository.ReviewsRepository;
 
@@ -46,14 +42,21 @@ public class ReviewsAPI {
 
 	@GetMapping("/{id}")
 	public ReviewsDTO getReview(@PathVariable("id") int id) {
+	
+		try {
+			if (id < 0) {
+				throw new RuntimeException("ID non valido");
+			}
 		Optional<Reviews> review = revirepo.findById(id);
 		if (review.isPresent()) {
 			return rmapper.daReviewADTO(review.get());
 
-		} else {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Review non trovata");
 		}
-
+		
+		}catch(Exception e) {
+			throw new RuntimeException("Review non trovata");
+	}return null;
+	    
 	}
 
 	@GetMapping()
@@ -84,19 +87,19 @@ public class ReviewsAPI {
 	public ResponseEntity<ReviewsDTO> updateReview(@PathVariable("id")int id, @RequestBody ReviewsDTO dto){
 	Optional<Reviews> reviewOptional = revirepo.findById(id);
     if (reviewOptional.isEmpty()) {
-        // Se il gioco non esiste, restituisco 404 NOT FOUND
+       
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    // Aggiorno l'oggetto esistente con i dati del DTO
+   
     Reviews existingReview = reviewOptional.get();
-    existingReview = rmapper.daDTOAReview(dto); // Questo può essere modificato per preservare l'ID e altri campi necessari
-    existingReview.setId(id); // Assicuro che l'ID del gioco rimanga invariato
+    existingReview = rmapper.daDTOAReview(dto); 
+    existingReview.setId(id); 
 
-    // Salvo il gioco aggiornato
+ 
    Reviews updatedReview = revirepo.save(existingReview);
 
-    // Restituisco il DTO del gioco aggiornato con status 200 OK
+  
     return new ResponseEntity<>(rmapper.daReviewADTO(updatedReview),HttpStatus.OK);
 }
 	
